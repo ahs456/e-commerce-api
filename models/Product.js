49fb7c0f7a1a335bd6b3911ca348b9d67d49c36d
Axh <anikahussain1@outlook.com>
2023-08-27 18:14:43 +0100

    set up feilds and rules for Product model

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,28 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
